d984e43234b73a6f1955b6e5a446918c33eff1cc
perfectrider <konsgrebenev@gmail.com>
2023-01-12 17:22:06 +0300

    Recursion with caching.

---
+++ Algorithms/fib recursion with memorization.py
@@ -0,0 +1,17 @@
+
+# Fibonacci sequence with recursion and caching?
+
+cache = []
+
+def fib_ind(index):
+
+    if index <= 1:
+        result = index
+    else:
+        result = fib_ind(index - 1) + fib_ind(index - 2)
+        cache.append(result)
+    return result
+
+print(fib_ind(30))
+
+# This func is more slowly than first var.
