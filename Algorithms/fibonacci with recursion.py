4f3746c2689e3f2882c5b733f883a580eaefdd7d
perfectrider <konsgrebenev@gmail.com>
2023-01-12 17:04:07 +0300

    Refactoring

--- Algorithms/fibonacci with recursion.py
+++ Algorithms/fibonacci with recursion.py
@@ -1,18 +1,16 @@
 
 # Fibonacci sequence with recursion.
 
-digs2 = [0, 1]
+digs = [0, 1]
 
 def fib_ind(index):
 
-    digs2.append(digs2[-1] + digs2[-2])
-    length = len(digs2)
-    if index >= 1 and index <= length:
-
-        return digs2[index - 1]
+    digs.append(digs[-1] + digs[-2])
+    if index >= 1 and index <= len(digs):
+        return digs[index - 1]
 
     return fib_ind(index)
 
-print(fib_ind(1))
+print(fib_ind(35))
 
 # Possibility to implement recursion with a local variable?
