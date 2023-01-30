39aba10bf2b4dd660513e7d36d64e131c200ec93
perfectrider <konsgrebenev@gmail.com>
2023-01-30 12:42:35 +0300

    asynch with generators realized, asycnh corutine realized

---
+++ gil_and_concurency/async_corutin.py
@@ -0,0 +1,55 @@
+# 1. Corutins, asynch(), await()
+from inspect import getgeneratorstate
+
+
+# def subgen():
+#     message = yield
+#     print('Subgen received: ', message)
+#
+# a = subgen()
+# print(getgeneratorstate(a)) # GEN_CREATED
+# a.send(None)
+# print(getgeneratorstate(a)) # GEN_SUSPENDED (Приостановлен)
+# a.send('Hello-1')   # Subgen received:  Hello-1, -> StopIteration
+
+
+def subgen():
+    x = 'Ready to accept message'
+    message = yield x
+    print('Subgen received: ', message)
+
+a = subgen()
+a.send(None)  # if print - "Ready to accept message"
+# a.send('Hello-2')   # Subgen received:  Hello-2, -> StopIteration
+
+
+# 2.
+
+class MyException(Exception):
+    pass
+
+def average():
+    count = 0
+    sum = 0
+    average = None
+
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print('Done')
+        except MyException:
+            print('MyException raised!')
+        else:
+            count += 1
+            sum += x
+            average = round(sum / count, 2)
+
+# obj.send(3)
+# 3.0
+# obj.send(5)
+# 4.0
+#...
+# obj.throw(StopIteration)
+# 'Done'
+# 4.0
--- gil_and_concurency/async_gen_2.py
+++ gil_and_concurency/async_gen_2.py
@@ -1,6 +1,11 @@
 # 1. Asynchronous behavior with generators
 
 import socket
+from select import select
+
+tasks = []
+to_read = {}
+to_write = {}
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,19 +14,51 @@ def server():
     server_socket.listen()
 
     while True:
+        yield ('read', server_socket)
         client_socket, adress = server_socket.accept()
         print('connection from', adress)
-        client(client_socket)
+        tasks.append(client(client_socket))
 
-def client():
+def client(client_socket):
     while True:
-        print('before .request()')
+        yield ('read', client_socket)
         request = client_socket.recv(4096)
 
         if not request:
             break
         else:
             response = 'Hello world\n'.encode()
+            yield ('write', client_socket)
             client_socket.send(response)
 
     client_socket.close()
+
+
+def event_loop():
+    while any([tasks, to_read, to_write]):
+        while not tasks:
+            ready_to_read, ready_to_write, _ = select(to_read, to_write, [])
+
+            for sock in ready_to_read:
+                tasks.append(to_read.pop(sock))
+
+            for sock in ready_to_write:
+                tasks.append(to_write.pop(sock))
+
+        try:
+            task = tasks.pop(0)
+            reason, sock = next(task)
+            if reason == 'read':
+                to_read[sock] = task
+            if reason == 'write':
+                to_write[sock] = task
+
+        except StopIteration:
+            print('Done!')
+
+
+
+
+
+tasks.append(server())
+event_loop()
